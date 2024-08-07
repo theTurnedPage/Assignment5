edd4c66eb561bcf8c9ab048de474935d84aab1f8
Page <158675598+theTurnedPage@users.noreply.github.com>
2024-08-07 14:04:06 -0500

    Begin adding code to overriden methods from CustomList Interface

--- src/main/java/com/coderscampus/arraylist/CustomArrayList.java
+++ src/main/java/com/coderscampus/arraylist/CustomArrayList.java
@@ -1,24 +1,36 @@
 package com.coderscampus.arraylist;
 
+import java.util.Arrays;
+
 public class CustomArrayList<T> implements CustomList<T> {
 	Object[] items = new Object[10];
+	private int size;
 
 	@Override
 	public boolean add(T item) {
-		// TODO Auto-generated method stub
+		
+		//backSize();
+		items[size++] = item;		
 		return false;
 	}
 
 	@Override
-	public int getSize() {
-		// TODO Auto-generated method stub
-		return 0;
+	public int getSize() {		
+		return size;
 	}
 
 	@Override
 	public T get(int index) {
-		// TODO Auto-generated method stub
 		return null;
+	}
+		
+	private void backSize() {
+		
+		if (size == items.length) {
+			//double size of array
+			items = Arrays.copyOf(items, items.length * 2);
+		}
+		
 	}
 	
 }
